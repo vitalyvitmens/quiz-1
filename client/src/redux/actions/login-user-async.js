7a2c3d252a75e87e243940e0eb0661dbe5381f0c
vitalyvitmens <otm_@tut.by>
2023-11-08 16:29:03 +0300

    add data in project

--- client/src/redux/actions/login-user-async.js
+++ client/src/redux/actions/login-user-async.js
@@ -3,11 +3,6 @@ import axios from '../../utils/axios'
 
 export const loginUserAsync = ({ login, password }) => {
 	return async (dispatch) => {
-		// const loginUserData = await axios.post('/auth/login', {
-		// 	method: 'POST',
-		// 	headers: { 'Content-Type': 'application/json' },
-		// 	body: JSON.stringify({ login, password }),
-		// })
 		const { data } = await axios.post('/auth/login', {
 			login,
 			password,
@@ -19,36 +14,3 @@ export const loginUserAsync = ({ login, password }) => {
 		return dispatch(loginUser(data))
 	}
 }
-
-// export const getMeAsync = () => {
-// 	return async (dispatch) => {
-// 		const response = await axios.get(`/auth/me`, {
-// 			method: 'GET',
-// 		})
-// 		const data = await response.json()
-// 		return dispatch(getMe(data))
-// 	}
-// }
-
-// export const loginUserAsync = () => {
-// 	return async (dispatch) => {
-// 		const response = await fetch(
-// 			'auth/loginUser',
-// 			async ({ login, password }) => {
-// 				try {
-// 					const { data } = await axios.post('/auth/login', {
-// 						login,
-// 						password,
-// 					})
-
-// 					if (data.token) window.localStorage.setItem('token', data.token)
-// 					return data
-// 				} catch (error) {
-// 					console.log(error)
-// 				}
-// 			}
-// 		)
-// 		const loginUserData = await response.json()
-// 		return dispatch(loginUser(loginUserData))
-// 	}
-// }
--- client/src/redux/actions/register-user-async.js
+++ client/src/redux/actions/register-user-async.js
@@ -3,11 +3,6 @@ import axios from '../../utils/axios'
 
 export const registerUserAsync = ({ login, password }) => {
 	return async (dispatch) => {
-		// const loginUserData = await axios.post('/auth/login', {
-		// 	method: 'POST',
-		// 	headers: { 'Content-Type': 'application/json' },
-		// 	body: JSON.stringify({ login, password }),
-		// })
 		const { data } = await axios.post('/auth/register', {
 			login,
 			password,
@@ -19,26 +14,3 @@ export const registerUserAsync = ({ login, password }) => {
 		return dispatch(registerUser(data))
 	}
 }
-
-// export const registerUserAsync = () => {
-// 	return async (dispatch) => {
-// 		const response = await fetch(
-// 			'auth/registerUser',
-// 			async ({ login, password }) => {
-// 				try {
-// 					const { data } = await axios.post('/auth/register', {
-// 						login,
-// 						password,
-// 					})
-
-// 					if (data.token) window.localStorage.setItem('token', data.token)
-// 					return data
-// 				} catch (error) {
-// 					console.log(error)
-// 				}
-// 			}
-// 		)
-// 		const registerUserData = await response.json()
-// 		return dispatch(registerUser(registerUserData))
-// 	}
-// }

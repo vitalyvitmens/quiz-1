4c46a2c8068f026e2cf93308d94987feee1b3959
vitalyvitmens <otm_@tut.by>
2023-11-08 15:32:03 +0300

    add data in project

--- client/src/redux/actions/get-me-async.js
+++ client/src/redux/actions/get-me-async.js
@@ -3,18 +3,7 @@ import { getMe } from './get-me'
 
 export const getMeAsync = () => {
 	return async (dispatch) => {
-		const getMeData = await axios.get(`/auth/me`, {
-			method: 'GET',
-		})
-		try {
-			// console.log('getMeData:', getMeData)
-			// console.log('getMeData.data.token:', getMeData.data?.token)
-			// console.log('getMeData.data.user._id:', getMeData.data.user?._id)
-			// console.log('getMeData.status:', getMeData?.status)
-			return dispatch(getMe(getMeData))
-		} catch (error) {
-			console.log(error)
-		}
+		const getMeData = await axios.get('/auth/me')
 		return dispatch(getMe(getMeData))
 	}
 }
